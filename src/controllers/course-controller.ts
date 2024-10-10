cf479f57d874c115580ff6ad7b71d1d9cdaa2742
Nicholas Tseng <nicolas@desygner.com>
2024-10-10 22:31:34 +1000

    create subscribe and unsubscribe function for course

--- src/controllers/course-controller.ts
+++ src/controllers/course-controller.ts
@@ -67,6 +67,29 @@ export async function getCourses(req: Request, res: Response) {
 	}
 }
 
+export async function getAllCourses(req: Request, res: Response) {
+	const token = req.headers.authorization?.split(' ')[1];
+
+	if (!token) return res.status(401).json({ message: 'Invalid token.' });
+
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+			userId: number;
+			role: 'educator' | 'learner';
+		};
+
+		const managerUserId = decoded.userId;
+
+		const sql = 'SELECT * FROM courses';
+
+		const [result] = await pool.execute(sql, [managerUserId]);
+
+		return res.json(result);
+	} catch (error) {
+		return res.status(500).json({ message: 'Internal server error' });
+	}
+}
+
 export function deleteCourse(req: Request, res: Response) {
 	const token = req.headers.authorization?.split(' ')[1];
 
@@ -151,5 +174,92 @@ export function updateCourse(req: Request, res: Response) {
 			);
 	} catch (error) {
 		return res.status(500).json({ message: 'Internal server error' });
+	}
+}
+
+export async function subscribeCourse(req: Request, res: Response) {
+	const token = req.headers.authorization?.split(' ')[1];
+
+	if (!token) {
+		return res.status(403).json({ message: 'Authentication required.' });
+	}
+
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+			userId: number;
+			role: 'educator' | 'learner';
+		};
+
+		if (decoded.role !== 'learner')
+			return res.status(403).json({ message: 'Forbidden' });
+
+		const studentId = decoded.userId;
+		const courseId = req.params.id;
+
+		const sql = `
+    	UPDATE courses
+    	SET student_ids = JSON_ARRAY_APPEND(
+      	COALESCE(student_ids, JSON_ARRAY()), 
+      	'$', 
+      	?
+    	)
+    	WHERE id = ?;
+  `;
+
+		pool
+			.execute(sql, [studentId, courseId])
+			.then(() =>
+				res.status(200).json({ message: 'Subscribed to course successfully' })
+			)
+			.catch((error) =>
+				res.status(500).json({ message: 'Error subscribing to course', error })
+			);
+	} catch (error) {
+		return res.status(500).json({ message: 'Internal server error' });
+	}
+}
+
+export async function unsubscribeCourse(req: Request, res: Response) {
+	const token = req.headers.authorization?.split(' ')[1];
+
+	if (!token)
+		return res.status(403).json({ message: 'Authentication required.' });
+
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+			userId: number;
+			role: 'educator' | 'learner';
+		};
+
+		if (decoded.role !== 'learner')
+			return res.status(403).json({ message: 'Forbidden' });
+
+		const studentId = decoded.userId;
+		const courseId = req.params.id;
+
+		const [rows]: any[] = await pool.execute(
+			'SELECT student_ids FROM courses WHERE id = ?',
+			[courseId]
+		);
+
+		const course = rows[0];
+
+		if (!course) return res.status(404).json({ message: 'Course not found' });
+
+		const updatedStudentIds = course.student_ids.filter(
+			(id: number) => id !== studentId
+		);
+
+		const sql = `
+      UPDATE courses
+      SET student_ids = ?
+      WHERE id = ?;
+    `;
+
+		await pool.execute(sql, [JSON.stringify(updatedStudentIds), courseId]);
+
+		res.status(200).json({ message: 'Unsubscribed from course successfully' });
+	} catch (error) {
+		return res.status(500).json({ message: 'Error processing request', error });
 	}
 }
--- src/routes/course-route.ts
+++ src/routes/course-route.ts
@@ -2,15 +2,21 @@ import { Router } from 'express';
 import {
 	createCourse,
 	getCourses,
+	getAllCourses,
 	deleteCourse,
 	updateCourse,
+	subscribeCourse,
+	unsubscribeCourse,
 } from '../controllers';
 
 const router = Router();
 
 router.post('/create-course', createCourse);
-router.get('/get-courses', getCourses);
+router.get('/get-courses/me', getCourses);
+router.get('/get-courses/all', getAllCourses);
 router.delete('/delete-course/:id', deleteCourse);
 router.put('/update-course/:id', updateCourse);
+router.put('/subscribe-course/:id', subscribeCourse);
+router.put('/unsubscribe-course/:id', unsubscribeCourse);
 
 export default router;
